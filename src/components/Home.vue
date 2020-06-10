5315d752758dd31dcb17ffdad79c985a146165a4
shogosakai <shogosakai73@gmail.com>
2020-06-10 20:48:58 +0900

    learn how to use local filter.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <p v-border:solid.round.shadow="{'width':'5px', 'color':'red' }">{{tmpData}}</p>
-    <h2>{{ title | upperCase }}</h2>
-    <p>{{ subTitle | upperCase }}</p>
+    <h2>{{ title | lowerCase }}</h2>
+    <p>{{ subTitle | lowerCase }}</p>
   </div>
 </template>
 
@@ -14,6 +14,11 @@ export default {
       title: "Welcome",
       subTitle: "my Blog"
       };
+  },
+  filters:{
+    lowerCase(value){
+      return value.toLowerCase();
+    }
   },
   directives:{
     border(el, binding){

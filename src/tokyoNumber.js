49e4cf3f3b2c690b1ea3171028dbf8e539074f01
shogosakai <shogosakai73@gmail.com>
2020-06-10 22:06:27 +0900

    learn how to make and use mixins.

---
+++ src/tokyoNumber.js
@@ -0,0 +1,15 @@
+export const tokyoNumber = {
+  data(){
+    return {
+      title: "Welcome",
+      subTitle: "my Blog",
+      number: 0
+      };
+  },
+  filters:{
+    lowerCase(value){
+      return value.toLowerCase();
+    }
+  },
+
+}

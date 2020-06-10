e9da17faea8c161d7e9afece444627aaf524deeb
shogosakai <shogosakai73@gmail.com>
2020-06-10 09:24:52 +0900

    change element style by using custom directive, custom modifiers.

--- src/main.js
+++ src/main.js
@@ -5,9 +5,15 @@ import LikeNumber from './components/LikeNumber.vue'
 Vue.config.productionTip = false
 Vue.component('LikeNumber',  LikeNumber)
 Vue.directive('border', function(el, binding){
-  el.style.border = 'solid blue 2px';
   el.style.borderWidth = binding.value.width;
   el.style.borderColor = binding.value.color;
+  el.style.borderStyle = binding.arg;
+  if(binding.modifiers.round){
+    el.style.borderRadius = '0.5rem';
+  }
+  if(binding.modifiers.shadow){
+    el.style.boxShadow = "0px 2px 5px rgba(0,0,0,0.26)";
+  }
   console.log(binding);
 });
 new Vue({

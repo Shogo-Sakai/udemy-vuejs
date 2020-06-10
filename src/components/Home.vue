27c09e7aac1418917a8c66a1e346d09e31836530
shogosakai <shogosakai73@gmail.com>
2020-06-10 09:33:21 +0900

    how to use local directives.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,25 @@
 <template>
-  <p v-border:solid.round.shadow="{'width':'5px', 'color':'red' }">Home</p>
+  <p v-border:solid.round.shadow="{'width':'5px', 'color':'red' }">{{tmpData}}</p>
 </template>
+
+<script>
+export default {
+  data(){
+    return {'tmpData':'hello'};
+  },
+  directives:{
+    border(el, binding){
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if(binding.modifiers.round){
+        el.style.borderRadius = '0.5rem';
+      }
+      if(binding.modifiers.shadow){
+        el.style.boxShadow = "0px 2px 5px rgba(0,0,0,0.26)";
+      }
+      console.log(binding);
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,20 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import LikeNumber from './components/LikeNumber.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.component('LikeNumber',  LikeNumber)
-Vue.directive('border', function(el, binding){
-  el.style.borderWidth = binding.value.width;
-  el.style.borderColor = binding.value.color;
-  el.style.borderStyle = binding.arg;
-  if(binding.modifiers.round){
-    el.style.borderRadius = '0.5rem';
-  }
-  if(binding.modifiers.shadow){
-    el.style.boxShadow = "0px 2px 5px rgba(0,0,0,0.26)";
-  }
-  console.log(binding);
-});
 new Vue({
   render: h => h(App),
 }).$mount('#app')
